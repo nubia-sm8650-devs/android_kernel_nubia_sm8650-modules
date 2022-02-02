81642a5992965a8721ff272ff8604aa1960c075d
dianlujitao <dianlujitao@lineageos.org>
2022-02-02 23:37:08 +0800

    techpack: audio: aw882xx: Load calibration file through firmware class
    Direct access to filesystem is insane and hits a neverallow, load it
    through the firmware class instead.
    
    Change-Id: I72149f8674c6211fab57a025b47cf84e505a256a

--- qcom/opensource/audio-kernel/dsp/aw882xx/aw882xx_calib.h
+++ qcom/opensource/audio-kernel/dsp/aw882xx/aw882xx_calib.h
@@ -166,7 +166,7 @@ struct aw_cali_desc {
 int aw882xx_cali_init(struct aw_cali_desc *cali_desc);
 void aw882xx_cali_deinit(struct aw_cali_desc *cali_desc);
 int aw882xx_cali_svc_get_cali_status(void);
-int aw882xx_cali_read_re_from_nvram(int32_t *cali_re, int32_t ch_index);
+int aw882xx_cali_read_re_from_nvram(struct device *dev, int32_t *cali_re, int32_t ch_index);
 
 
 #endif
