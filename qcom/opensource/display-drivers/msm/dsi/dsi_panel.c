cd9d666e8b7311a35ecf2b1df93494336e3f7c92
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-10 15:07:38 +0200

    techpack: display: dsi_panel: define HBM commands
    Change-Id: If910f52f209b52710a71d3211df786243369c362

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.c
@@ -1903,6 +1903,8 @@ const char *cmd_set_prop_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-qsync-on-commands",
 	"qcom,mdss-dsi-qsync-off-commands",
 	"qcom,mdss-dsi-calibration-commands",
+	"qcom,mdss-dsi-hbm-on-command",
+	"qcom,mdss-dsi-hbm-off-command",
 };
 
 const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
@@ -1932,6 +1934,8 @@ const char *cmd_set_state_map[DSI_CMD_SET_MAX] = {
 	"qcom,mdss-dsi-qsync-on-commands-state",
 	"qcom,mdss-dsi-qsync-off-commands-state",
 	"qcom,mdss-dsi-calibration-commands-state",
+	"qcom,mdss-dsi-hbm-on-command-state",
+	"qcom,mdss-dsi-hbm-off-command-state",
 };
 
 int dsi_panel_get_cmd_pkt_count(const char *data, u32 length, u32 *cnt)
