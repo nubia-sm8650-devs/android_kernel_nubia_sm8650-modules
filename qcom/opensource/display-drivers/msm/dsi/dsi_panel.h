1c9cdc547bf32100c2eab3fa7bb482b3782dc16e
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-10 15:08:04 +0200

    techpack: display: dsi_panel: read FOD dimming LUT
    Change-Id: I23f7914ce0adf6c1942c09212516ed5f6b22d289

--- qcom/opensource/display-drivers/msm/dsi/dsi_panel.h
+++ qcom/opensource/display-drivers/msm/dsi/dsi_panel.h
@@ -217,6 +217,12 @@ struct dsi_panel_calib_data {
 	size_t len;
 };
 
+#define BRIGHTNESS_ALPHA_PAIR_LEN 2
+struct brightness_alpha_pair {
+	u16 brightness;
+	u8 alpha;
+};
+
 struct dsi_panel {
 	const char *name;
 	const char *type;
@@ -282,6 +288,9 @@ struct dsi_panel {
 
 	struct dsi_panel_ops panel_ops;
 	struct dsi_panel_calib_data calib_data;
+
+	struct brightness_alpha_pair *fod_dim_lut;
+	unsigned int fod_dim_lut_len;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
