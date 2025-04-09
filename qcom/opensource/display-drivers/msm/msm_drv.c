8662c64699f88d29e219879c1dcc9aa39f057abf
Cosmin Tanislav <demonsingur@gmail.com>
2025-04-09 17:59:48 +0300

    Revert "display-drivers: Import nubiadp drivers"
    This reverts commit cd294a30c16720308b84679931145690d24dd884.

--- qcom/opensource/display-drivers/msm/msm_drv.c
+++ qcom/opensource/display-drivers/msm/msm_drv.c
@@ -2421,10 +2421,6 @@ static int __init msm_drm_register(void)
 	msm_dsi_register();
 	msm_edp_register();
 	msm_hdmi_register();
-#if IS_ENABLED(CONFIG_NUBIA_DP)
-	nubia_usb_switch_dp_init();
-	nubia_dp_preference_init();
-#endif
 	return 0;
 }
 
@@ -2442,10 +2438,6 @@ static void __exit msm_drm_unregister(void)
 	dp_display_unregister();
 	dsi_display_unregister();
 	sde_rsc_unregister();
-#if IS_ENABLED(CONFIG_NUBIA_DP)
-	nubia_usb_switch_dp_exit();
-	nubia_dp_preference_exit();
-#endif
 	platform_driver_unregister(&msm_platform_driver);
 }
 
