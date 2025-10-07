adb831b00919961f9e247ceb8708e9445fc6a149
Giovanni Ricca <giovanniricca@proton.me>
2025-10-07 12:18:12 +0200

    Revert "msm: camera: common: Fix slab-out-of-bounds in cam_packet validate"
    This reverts commit 0a3490ad9f50331bc8832fc31539f54de8fcd81b.

--- qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_eeprom/cam_eeprom_core.c
+++ qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_eeprom/cam_eeprom_core.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2017-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025, Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023, Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/module.h>
@@ -1227,11 +1227,10 @@ static int32_t cam_eeprom_pkt_parse(struct cam_eeprom_ctrl_t *e_ctrl, void *arg)
 	int32_t                         rc = 0;
 	struct cam_control             *ioctl_ctrl = NULL;
 	struct cam_config_dev_cmd       dev_config;
-	uintptr_t                       generic_pkt_addr;
+	uintptr_t                        generic_pkt_addr;
 	size_t                          pkt_len;
 	size_t                          remain_len = 0;
 	struct cam_packet              *csl_packet = NULL;
-	struct cam_packet              *csl_packet_u = NULL;
 	struct cam_eeprom_soc_private  *soc_private =
 		(struct cam_eeprom_soc_private *)e_ctrl->soc_info.soc_private;
 	struct cam_sensor_power_ctrl_t *power_info = &soc_private->power_info;
@@ -1263,12 +1262,15 @@ static int32_t cam_eeprom_pkt_parse(struct cam_eeprom_ctrl_t *e_ctrl, void *arg)
 	}
 
 	remain_len -= (size_t)dev_config.offset;
-	csl_packet_u = (struct cam_packet *)
+	csl_packet = (struct cam_packet *)
 		(generic_pkt_addr + (uint32_t)dev_config.offset);
-	rc = cam_packet_util_copy_pkt_to_kmd(csl_packet_u, &csl_packet, remain_len);
-	if (rc) {
-		CAM_ERR(CAM_EEPROM, "Copying packet to KMD failed");
-		goto put_ref;
+
+	if (cam_packet_util_validate_packet(csl_packet,
+		remain_len)) {
+		CAM_ERR(CAM_EEPROM, "Invalid packet params");
+		cam_mem_put_cpu_buf(dev_config.packet_handle);
+		rc = -EINVAL;
+		return rc;
 	}
 
 	switch (csl_packet->header.op_code & 0xFFFFFF) {
@@ -1278,7 +1280,8 @@ static int32_t cam_eeprom_pkt_parse(struct cam_eeprom_ctrl_t *e_ctrl, void *arg)
 					e_ctrl->soc_info.dev->of_node, e_ctrl);
 			if (rc < 0) {
 				CAM_ERR(CAM_EEPROM, "Failed: rc : %d", rc);
-				goto end;
+				cam_mem_put_cpu_buf(dev_config.packet_handle);
+				return rc;
 			}
 			rc = cam_eeprom_get_cal_data(e_ctrl, csl_packet);
 			if (rc)
@@ -1296,7 +1299,8 @@ static int32_t cam_eeprom_pkt_parse(struct cam_eeprom_ctrl_t *e_ctrl, void *arg)
 		if (rc) {
 			CAM_ERR(CAM_EEPROM,
 				"Failed in parsing the pkt");
-			goto end;
+			cam_mem_put_cpu_buf(dev_config.packet_handle);
+			return rc;
 		}
 
 		e_ctrl->cal_data.mapdata =
@@ -1357,7 +1361,8 @@ static int32_t cam_eeprom_pkt_parse(struct cam_eeprom_ctrl_t *e_ctrl, void *arg)
 			csl_packet, e_ctrl);
 		if (rc < 0) {
 			CAM_ERR(CAM_EEPROM, "Failed: rc : %d", rc);
-			goto end;
+			cam_mem_put_cpu_buf(dev_config.packet_handle);
+			return rc;
 		}
 
 		rc = cam_eeprom_power_up(e_ctrl,
@@ -1378,7 +1383,8 @@ static int32_t cam_eeprom_pkt_parse(struct cam_eeprom_ctrl_t *e_ctrl, void *arg)
 			e_ctrl->eebin_info.size);
 		if (rc < 0) {
 			CAM_ERR(CAM_EEPROM, "Failed in erase : %d", rc);
-			goto end;
+			cam_mem_put_cpu_buf(dev_config.packet_handle);
+			return rc;
 		}
 
 		/* Buffer time margin */
@@ -1387,7 +1393,8 @@ static int32_t cam_eeprom_pkt_parse(struct cam_eeprom_ctrl_t *e_ctrl, void *arg)
 		rc = cam_eeprom_write(e_ctrl);
 		if (rc < 0) {
 			CAM_ERR(CAM_EEPROM, "Failed: rc : %d", rc);
-			goto end;
+			cam_mem_put_cpu_buf(dev_config.packet_handle);
+			return rc;
 		}
 
 		rc = cam_eeprom_power_down(e_ctrl);
@@ -1404,9 +1411,6 @@ static int32_t cam_eeprom_pkt_parse(struct cam_eeprom_ctrl_t *e_ctrl, void *arg)
 		rc = -EINVAL;
 		break;
 	}
-end:
-	cam_common_mem_free(csl_packet);
-put_ref:
 	cam_mem_put_cpu_buf(dev_config.packet_handle);
 	return rc;
 power_down:
@@ -1423,7 +1427,6 @@ error:
 	e_ctrl->cal_data.num_data = 0;
 	e_ctrl->cal_data.num_map = 0;
 	e_ctrl->cam_eeprom_state = CAM_EEPROM_ACQUIRE;
-	cam_common_mem_free(csl_packet);
 	return rc;
 }
 
--- qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_tpg/cam_tpg_core.c
+++ qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_tpg/cam_tpg_core.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include "cam_tpg_core.h"
@@ -665,7 +665,6 @@ static int cam_tpg_packet_parse(
 	uintptr_t generic_ptr;
 	size_t len_of_buff = 0, remain_len = 0;
 	struct cam_packet *csl_packet = NULL;
-	struct cam_packet *csl_packet_u = NULL;
 
 	rc = cam_mem_get_cpu_buf(config->packet_handle,
 		&generic_ptr, &len_of_buff);
@@ -685,11 +684,13 @@ static int cam_tpg_packet_parse(
 	}
 	remain_len = len_of_buff;
 	remain_len -= (size_t)config->offset;
-	csl_packet_u = (struct cam_packet *)(generic_ptr +
+	csl_packet = (struct cam_packet *)(generic_ptr +
 		(uint32_t)config->offset);
-	rc = cam_packet_util_copy_pkt_to_kmd(csl_packet_u, &csl_packet, remain_len);
-	if (rc) {
-		CAM_ERR(CAM_TPG, "Copying packet to KMD failed");
+
+	if (cam_packet_util_validate_packet(csl_packet,
+		remain_len)) {
+		CAM_ERR(CAM_TPG, "Invalid packet params");
+		rc = -EINVAL;
 		goto end;
 	}
 
@@ -705,12 +706,12 @@ static int cam_tpg_packet_parse(
 		if (csl_packet->num_cmd_buf <= 0) {
 			CAM_ERR(CAM_TPG, "Expecting atleast one command in Init packet");
 			rc = -EINVAL;
-			goto free_kdup;
+			goto end;
 		}
 		rc = cam_tpg_cmd_buf_parse(tpg_dev, csl_packet);
 		if (rc < 0) {
 			CAM_ERR(CAM_TPG, "CMD buffer parse failed");
-			goto free_kdup;
+			goto end;
 		}
 		if (!tpg_dev->hw_no_ops)
 			tpg_hw_config(&tpg_dev->tpg_hw,
@@ -724,7 +725,7 @@ static int cam_tpg_packet_parse(
 		rc = cam_tpg_cmd_buf_parse(tpg_dev, csl_packet);
 		if (rc < 0) {
 			CAM_ERR(CAM_TPG, "CMD buffer parse failed");
-			goto free_kdup;
+			goto end;
 		}
 
 		CAM_DBG(CAM_TPG, "TPG[%d] packet request id : %llu",
@@ -751,8 +752,6 @@ static int cam_tpg_packet_parse(
 		rc = -EINVAL;
 		break;
 	}
-free_kdup:
-	cam_common_mem_free(csl_packet);
 end:
 	cam_mem_put_cpu_buf(config->packet_handle);
 	return rc;
